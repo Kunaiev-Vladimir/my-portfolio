eb467945a15869e9df698eddbb3d029eb0c96812
Vladimir Kunaiev <140187@ukr.net>
2026-06-15 15:29:53 +0300

    15_06_2026

--- my_portfolio/settings.py
+++ my_portfolio/settings.py
@@ -33,7 +33,11 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG') == 'True'
 
 #  ALLOWED_HOSTS = []
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+#  ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = os.getenv(
+    'ALLOWED_HOSTS',
+    '127.0.0.1,localhost'
+).split(',')
 
 
 # Application definition
